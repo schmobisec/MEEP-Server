e01631512fbbc8fa5f7cddf9d6bd8dc2b13fc048
livefacter <qwiny4962@gmail.com>
2017-08-16 21:35:40 +0900

    google_oauth 로그인 버튼(image) 수정

--- modules/google_oauth/index.php
+++ modules/google_oauth/index.php
@@ -68,7 +68,7 @@ if (isset($authUrl)){
 	echo '<div align="center">';
 	echo '<h2>Google OAuth 2.0 Login</h2>';
 	echo '<div>Google 아이콘 누르세요.</div>';
-	echo '<a class="login" href="' . $authUrl . '"><img src="images/google-login-button.png" /></a>';
+	echo '<a class="login" href="' . $authUrl . '"><img src="modules/google_oauth/images/google-login-button.png" /></a>';
 	echo '</div>';
 
 } else {
