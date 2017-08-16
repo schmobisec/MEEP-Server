79de80628367fe5a576d3c32223570d65a6dd30f
livefacter <qwiny4962@gmail.com>
2017-08-17 00:19:14 +0900

    json 파일 수정 - 파일 경로, exception 영어 전환

--- modules/json/cert_json.php
+++ modules/json/cert_json.php
@@ -1,29 +1,29 @@
 <?php
-include_once '../google/connect.php';
+include_once '../google_oauth/connect.php';
 
 //인증서 검색
-$sql = "SELECT google_der_path FROM google_cert WHERE google_email='qwiny4962@gmail.com'";
+$sql = "SELECT google_crt_path FROM google_cert WHERE google_email='qwiny4962@gmail.com'";
 $result = mysql_query($sql,$db);
 if(!$result) {
- throw new Exception('유효하지 않는 쿼리입니다.');
+ throw new Exception('Invalid DB query');
 }
 
 $row = mysql_fetch_array($result);
 
 if($row <= 0) {
- throw new Exception('인증서가 존재하지 않습니다.');
+ throw new Exception('The certificate does not exist');
  echo  "<script>alert('인증서가 존재하지 않습니다.')</script>";
 }
 
 //인증서 파일 base64 인코딩
-$data = file_get_contents('../' . $row[google_der_path]);
+$data = file_get_contents('../../' . $row[google_crt_path]);
 
 //인증서 파일 읽기 여부 확인
 if($data !== false) {
  $base64 = 'data:application/octet-stream' . ';base64,' . base64_encode($data);
 }
 else {
- throw new Exception('인증서를 읽지 못했습니다.');
+ throw new Exception('Could not read certificate');
 }
 
 //json 파일 변환
